93097706b298235a7bc614f78421bee9aeb4e51f
GPR <rochafrancoagustin@hotmail.com>
2022-02-23 19:29:51 -0300

    inicio

---
+++ src/ejerciciotienda/EjercicioTienda.java
@@ -0,0 +1,14 @@
+package ejerciciotienda;
+
+public class EjercicioTienda {
+
+    public static void main(String[] args) {
+        
+        Producto producto = new Producto(0,"Manopla",420,69);
+        
+        System.out.println(producto);
+        
+    }
+    
+}
+    
---
+++ src/ejerciciotienda/Producto.java
@@ -0,0 +1,53 @@
+package ejerciciotienda;
+
+public class Producto {
+    
+    int codigo;
+        String nombre;
+        double precio;
+        int codigoFabricante;
+
+        public Producto(int codigo, String nombre, double precio, int codigoFabricante) {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.precio = precio;
+            this.codigoFabricante = codigoFabricante;
+        }
+
+        public Producto() {
+        }
+
+        public int getCodigo() {
+            return codigo;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public double getPrecio() {
+            return precio;
+        }
+
+        public int getCodigoFabricante() {
+            return codigoFabricante;
+        }
+
+        public void setCodigo(int codigo) {
+            this.codigo = codigo;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public void setPrecio(double precio) {
+            this.precio = precio;
+        }
+
+        public void setCodigoFabricante(int codigoFabricante) {
+            this.codigoFabricante = codigoFabricante;
+        } 
+
+    }   
+    
