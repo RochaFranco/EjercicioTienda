cdf82f5b4adfc4d62ba266b4eb89115e90a79930
GPR <rochafrancoagustin@hotmail.com>
2022-02-23 21:11:57 -0300

    se conecto a la base de datos y se mostraron los datos de esta

--- src/ejerciciotienda/EjercicioTienda.java
+++ src/ejerciciotienda/EjercicioTienda.java
@@ -8,7 +8,9 @@ import java.util.List;
 
 public class EjercicioTienda {
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        
+        ServicioProducto servicio = new ServicioProducto();
      
         Connection connection = null;
         
@@ -25,6 +27,27 @@ public class EjercicioTienda {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        
+        String sql = "SELECT * FROM producto";
+        
+        Statement stmt = connection.createStatement();
+        
+        ResultSet rs = stmt.executeQuery(sql);
+        
+        while(rs.next()){
+            int codigo = rs.getInt("codigo");
+            
+            String nombre = rs.getString("nombre");
+            
+            double precio = rs.getDouble("precio");
+            
+            int codigoFabricante = rs.getInt("codigo_fabricante");
+            
+            Producto producto = new Producto(codigo, nombre, precio, codigoFabricante);
+            
+            servicio.mostrarProducto(producto);
+            
         }
         
     }
