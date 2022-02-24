7f5676f2b9806934c20146a94e42488b6371d227
GPR <rochafrancoagustin@hotmail.com>
2022-02-23 21:59:30 -0300

    Se agrego la clase servicioMenu en el package servicios

--- src/ejerciciotienda/EjercicioTienda.java
+++ src/ejerciciotienda/EjercicioTienda.java
@@ -2,6 +2,7 @@ package ejerciciotienda;
 
 import ejerciciotienda.entidades.Producto;
 import ejerciciotienda.persistencia.DAOProducto;
+import ejerciciotienda.servicios.ServicioMenu;
 import ejerciciotienda.servicios.ServicioProducto;
 import java.sql.*;
 import java.util.ArrayList;
@@ -12,53 +13,12 @@ public class EjercicioTienda {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         
-        ServicioProducto servicio = new ServicioProducto();
-       DAOProducto dao = new DAOProducto();
+        ServicioProducto servicioProducto = new ServicioProducto();
+        DAOProducto dao = new DAOProducto();
+        ServicioMenu servicioMenu = new ServicioMenu(servicioProducto,dao);
        
-       Scanner scanner = new Scanner(System.in);
-       
-       List<Producto> productos = dao.obtenerLista();
-       
-        System.out.println("a) Lista el nombre de todos los productos que hay en la tabla producto.");
-        System.out.println("b) Lista los nombres y los precios de todos los productos de la tabla producto.");
-        System.out.println("c) Listar aquellos productos que su precio esté entre 120 y 202.");
-        System.out.println("d) Buscar y listar todos los Portátiles de la tabla producto.");
-        System.out.println("e) Listar el nombre y el precio del producto más barato.");
-        System.out.println("f) Ingresar un producto a la base de datos.");
-        System.out.println("g) Ingresar un fabricante a la base de datos");
-        System.out.println("h) Editar un producto con datos a elección.");
-        System.out.print("Ingrese la opcion: ");
-        String opcion = scanner.next();
-        
-        switch(opcion)
-        {
-            case "a":
-                break;
-                
-            case "b": 
-                break;
-                
-            case "c":
-                break;
-                
-            case "d":
-                break;
-                
-            case "e":
-                break;
-                
-            case "f":
-                break;
-                
-            case "g":
-                break;
-                
-            case "h":
-                break;
-                
-                default: System.out.println("Opcion incorrecta");
-                break;
-        }
+   
+        servicioMenu.mostrarYEjecutarMenu();
      
      
         
---
+++ src/ejerciciotienda/servicios/ServicioMenu.java
@@ -0,0 +1,65 @@
+package ejerciciotienda.servicios;
+import ejerciciotienda.persistencia.DAOProducto;
+import java.util.*;
+
+public class ServicioMenu  {
+    
+    private final ServicioProducto servicioProducto;
+    private final DAOProducto dao;
+    
+    
+
+    public ServicioMenu(ServicioProducto servicioProducto, DAOProducto dao) {
+        this.servicioProducto = servicioProducto;
+        this.dao = dao;
+        
+    }
+    
+    public void mostrarYEjecutarMenu(){
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("a) Lista el nombre de todos los productos que hay en la tabla producto.");
+        System.out.println("b) Lista los nombres y los precios de todos los productos de la tabla producto.");
+        System.out.println("c) Listar aquellos productos que su precio esté entre 120 y 202.");
+        System.out.println("d) Buscar y listar todos los Portátiles de la tabla producto.");
+        System.out.println("e) Listar el nombre y el precio del producto más barato.");
+        System.out.println("f) Ingresar un producto a la base de datos.");
+        System.out.println("g) Ingresar un fabricante a la base de datos");
+        System.out.println("h) Editar un producto con datos a elección.");
+        System.out.print("Ingrese la opcion: ");
+        String opcion = scanner.next();
+        
+        switch(opcion)
+        {
+            case "a": 
+                break;
+                
+            case "b": 
+                break;
+                
+            case "c":
+                break;
+                
+            case "d":
+                break;
+                
+            case "e":
+                break;
+                
+            case "f":
+                break;
+                
+            case "g":
+                break;
+                
+            case "h":
+                break;
+                
+                default: System.out.println("Opcion incorrecta");
+                break;
+        }
+    }
+    
+    
+}
