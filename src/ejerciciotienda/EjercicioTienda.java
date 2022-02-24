f815c4836e1e21f453d906d18fa53b3f47474db6
GPR <rochafrancoagustin@hotmail.com>
2022-02-23 21:25:47 -0300

    Se agrego el package persistencia, se ordeno y mejoro la logica de la conexion de base de datos (DAO) y se creo el metodo obtenerLista

--- src/ejerciciotienda/EjercicioTienda.java
+++ src/ejerciciotienda/EjercicioTienda.java
@@ -1,6 +1,7 @@
 package ejerciciotienda;
 
 import ejerciciotienda.entidades.Producto;
+import ejerciciotienda.persistencia.DAOProducto;
 import ejerciciotienda.servicios.ServicioProducto;
 import java.sql.*;
 import java.util.ArrayList;
@@ -11,44 +12,12 @@ public class EjercicioTienda {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         
         ServicioProducto servicio = new ServicioProducto();
-     
-        Connection connection = null;
+       DAOProducto dao = new DAOProducto();
+       
+       List<Producto> productos = dao.obtenerLista();
+       
+     servicio.mostrarListaDeProductos(productos);
         
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/tienda?useSSL=false";
-            connection = DriverManager.getConnection(url, "root", "root");   
-            System.out.println("Ingrese");
-        }
-        catch (SQLException ex)
-        {
-            connection = null;
-            ex.printStackTrace();
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
-        }
-        
-        String sql = "SELECT * FROM producto";
-        
-        Statement stmt = connection.createStatement();
-        
-        ResultSet rs = stmt.executeQuery(sql);
-        
-        while(rs.next()){
-            int codigo = rs.getInt("codigo");
-            
-            String nombre = rs.getString("nombre");
-            
-            double precio = rs.getDouble("precio");
-            
-            int codigoFabricante = rs.getInt("codigo_fabricante");
-            
-            Producto producto = new Producto(codigo, nombre, precio, codigoFabricante);
-            
-            servicio.mostrarProducto(producto);
-            
-        }
         
     }
     
